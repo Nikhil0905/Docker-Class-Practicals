7608dfd97bd356aa86e2b4552a230065b9e5b947
Nikhil Shakya <134042174+Nikhil0905@users.noreply.github.com>
2026-04-02 01:42:18 +0530

    Add files via upload

---
+++ Automatic-dockerfile/app.py
@@ -0,0 +1,5 @@
+print("Just learnt to make docker file and docker compose using docker init cmmd")
+
+print("Trying to test and make it")
+
+print("If i am getting this printed that simply means yes the the file has been created successfully")
