8533745ff08321415cc95365c3d609ceb2056916
Nikhil Shakya <134042174+Nikhil0905@users.noreply.github.com>
2026-04-22 13:06:05 +0530

    Add files via upload

---
+++ maven-hello/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class App {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven!");
+        int result = add(10, 20);
+        System.out.println("10 + 20 = " + result);
+    }
+}
